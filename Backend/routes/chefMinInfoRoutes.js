fcc6f963bbc1a88bb15ea157d58a6c0cfbcd3c35
Tejas Pawar <134952886+Tejas-040303@users.noreply.github.com>
2025-05-23 21:57:30 +0530

    Delete Backend directory
    Odd backend, no use

--- Backend/routes/chefMinInfoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getChefsForDashboard } = require('../controllers/chefForCustomerController.js');
-
-// GET all chefs for customer dashboard
-router.get('/dashboard/chefDetails', getChefsForDashboard);
-
-module.exports = router;
